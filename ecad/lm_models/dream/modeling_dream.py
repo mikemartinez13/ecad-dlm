f3a3fc843292e776adeadb897392d768ab5e051e
mikemartinez13 <michaelcmartinez13@gmail.com>
2026-02-18 12:54:06 -0500

    set use_cache=False

--- ecad/lm_models/dream/modeling_dream.py
+++ ecad/lm_models/dream/modeling_dream.py
@@ -632,7 +632,8 @@ class DreamBaseModel(DreamPreTrainedModel):
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
-        use_cache = use_cache if use_cache is not None else self.config.use_cache
+        # use_cache = use_cache if use_cache is not None else self.config.use_cache
+        use_cache = False # temporary forcing 
 
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
